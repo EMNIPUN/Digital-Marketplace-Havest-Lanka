aa428e4f69f313fb758f0ef88843ca602c795f81
Sadeesha Sathsara <sadeeshasathsara99@gmail.com>
2025-03-17 11:46:57 +0530

    Login fixed

--- frontend/src/components/userManagement/dashboard/Body.jsx
+++ frontend/src/components/userManagement/dashboard/Body.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import Token from '../logins/Token'
 
 function Body() {
+    console.log(Token().userId)
     return (
         <div>Body</div>
     )
--- frontend/src/routes/LoginRoutes.jsx
+++ frontend/src/routes/LoginRoutes.jsx
@@ -3,15 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../components/userManagement/logins/login";
 import LoginPortal from "../components/userManagement/logins/loginPortal";
 import AdminImg from "../assets/userManagement/admin.jpg"
+import ShopImg from "../assets/userManagement/shop.jpg"
+import FarmerImg from "../assets/userManagement/farmer.jpg"
+import FinanceImg from "../assets/userManagement/finance.jpg"
+import DriverImg from "../assets/userManagement/driver.jpg"
 
 function LoginRoutes() {
     return (
         <Routes>
             <Route path="/admin-login" element={<Login role="Admin" img={AdminImg} />}></Route>
-            <Route path="/farmer-login" element={<Login role="Farmer" img={AdminImg} />}></Route>
-            <Route path="/shopowner-login" element={<Login role="Shop Owner" img={AdminImg} />}></Route>
-            <Route path="/driver-login" element={<Login role="Driver" img={AdminImg} />}></Route>
-            <Route path="/financemanager-login" element={<Login role="Admin" img={AdminImg} />}></Route>
+            <Route path="/farmer-login" element={<Login role="Farmer" img={FarmerImg} />}></Route>
+            <Route path="/shopowner-login" element={<Login role="Shop Owner" img={ShopImg} />}></Route>
+            <Route path="/driver-login" element={<Login role="Driver" img={DriverImg} />}></Route>
+            <Route path="/financemanager-login" element={<Login role="Finance Manager" img={FinanceImg} />}></Route>
 
             <Route path="/portal" element={<LoginPortal />} ></Route>
         </Routes>
