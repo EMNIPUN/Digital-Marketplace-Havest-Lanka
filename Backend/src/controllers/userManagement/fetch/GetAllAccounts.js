c80572f64e4d691b3cfc9c556be0f28aa0d7924c
Sadeesha Sathsara <sadeeshasathsara99@gmail.com>
2026-09-24 23:55:29 +0530

    fix(api): add return statements in GetAllAccounts to prevent process crash

--- Backend/src/controllers/userManagement/fetch/GetAllAccounts.js
+++ Backend/src/controllers/userManagement/fetch/GetAllAccounts.js
@@ -5,11 +5,11 @@ const GetAllAccounts = async (req, res) => {
     try {
         const accounts = await User.find()
         if (accounts.length === 0) {
-            res.status(200).json({ message: "No accounts found" })
+            return res.status(200).json({ message: "No accounts found" });
         }
-        res.status(200).json({ accounts: accounts })
+        return res.status(200).json({ accounts: accounts });
     } catch (e) {
-        res.status(500).json({ error: e.message })
+        return res.status(500).json({ error: e.message });
     }
 }
 
